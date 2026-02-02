4209fd510279d4f4d54d1dc65e13e5fda58024df
nithyasree694-bit <nithyasree694@gmail.com>
2026-02-02 04:32:15 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,41 +1,70 @@
+# variables.tf
+
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region where resources will be created"
   type        = string
-  default     = "ap-south-1" # CHANGE if you prefer
+  default     = "ap-south-1"
 }
 
-variable "instance_count" {
-  description = "Number of web servers to create"
+variable "apache_instance_count" {
+  description = "Number of Apache web server instances"
   type        = number
   default     = 2
+  
+  validation {
+    condition     = var.apache_instance_count > 0 && var.apache_instance_count <= 10
+    error_message = "Apache instance count must be between 1 and 10."
+  }
+}
+
+variable "nginx_instance_count" {
+  description = "Number of Nginx web server instances"
+  type        = number
+  default     = 2
+  
+  validation {
+    condition     = var.nginx_instance_count > 0 && var.nginx_instance_count <= 10
+    error_message = "Nginx instance count must be between 1 and 10."
+  }
 }
 
 variable "instance_type" {
-  description = "EC2 type"
+  description = "EC2 instance type for web servers"
   type        = string
-  default     = "t2.micro" # Free tier friendly
+  default     = "t3.micro"
 }
 
-variable "key_pair_name" {
-  description = "Name of an existing AWS EC2 key pair"
+variable "ami_id" {
+  description = "AMI ID for Ubuntu 22.04 LTS in the specified region"
   type        = string
-  # No default. Must set from Jenkins or tfvars.
+  default     = "ami-019715e0d74f695be"  # Ubuntu 22.04 LTS in us-east-1
+  
 }
 
-variable "name_prefix" {
-  description = "Name prefix for instances"
+variable "jenkins_ip" {
+  description = "Public IP address of Jenkins server (for SSH access)"
   type        = string
-  default     = "web"
+  
+  validation {
+    condition     = can(regex("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$", var.jenkins_ip))
+    error_message = "Jenkins IP must be a valid IPv4 address."
+  }
 }
 
-variable "env" {
-  description = "Environment tag"
+variable "public_key_path" {
+  description = "Path to SSH public key file for web server access"
   type        = string
-  default     = "dev"
+  default     = "/var/lib/jenkins/.ssh/webserver-key.pub"
 }
 
-variable "ansible_inventory_path" {
-  description = "Where to write hosts.ini (relative to this repo root in Jenkins workspace)"
+variable "environment" {
+  description = "Environment name (dev, staging, production)"
   type        = string
-  default     = "../ansible-playbooks/inventory/hosts.ini"
+  default     = "production"
+}
+
+variable "project_name" {
+  description = "Project name for resource tagging"
+  type        = string
+  default     = "devops-webapp"
 }
